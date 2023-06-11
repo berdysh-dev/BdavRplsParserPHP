68941b26b449d22c5e0a2b7030f142b9e6bf1691
root <root@berdysh.net>
2023-06-11 22:15:13 +0900

    Test

---
+++ tests/BdavRplsParserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once "vendor/autoload.php" ;
+
+    class BdavRplsParserTest extends PHPUnit\Framework\TestCase{
+
+
+
+        public function testAll(){
+            echo "\nTest.!!!\n" ;
+            $this->assertTrue(TRUE) ;
+        }
+    }
+
+
+
+
+
+
+
+
